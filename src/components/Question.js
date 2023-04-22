e3a7eedcdb6d4fc1f29ee8b98a4d7399e14e02e9
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-04-22 20:20:59 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
+  useEffect( ()=> {
+        const intervalId = setTimeout( () => setTimeRemaining(timeRemaining-1), 1000);
+
+        return function cleanup() {
+          clearTimeout(intervalId);
+        }
+
+  })
+
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
+  }
+  function handleReset() {
+    setTimeRemaining(10);
+    onAnswered(false);
   }
 
   const { id, prompt, answers, correctIndex } = question;
@@ -24,7 +38,7 @@ function Question({ question, onAnswered }) {
           </button>
         );
       })}
-      <h5>{timeRemaining} seconds remaining</h5>
+      <h5>{timeRemaining===0 ? handleReset() : timeRemaining} seconds remaining</h5>
     </>
   );
 }
